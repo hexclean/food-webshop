9a8f531a5ffc13e4d07e10f871367d91ceca57cb
hexclean <erdosjozsef20@gmail.com>
2021-12-23 13:06:05 +0200

    Connecting Redux Toolkit

--- src/components/Products/ProductList/ProductList.js
+++ src/components/Products/ProductList/ProductList.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 // Components
 import ProductItem from "./ProductItem";
 import Card from "../Shared/Card";
 
 const Products = () => {
+  const products = useSelector(state => state.products.productList);
+  console.log(products);
   const renderProducts = () => {
     return (
       <>
