2cd146fc9f3ae92d92def8badef3d89cbc1fb8c4
hexclean <erdosjozsef20@gmail.com>
2021-12-27 22:03:33 +0200

    Adding active class to the category item

--- src/components/Products/Categories/CategoryItem.js
+++ src/components/Products/Categories/CategoryItem.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { productsActions } from "../../../store/products-slice";
+import { categoriesActions } from "../../../store/categories-slice";
 import "./Css/CategoryItem.css";
 
 const CategoryItem = ({ id, name }) => {
   const dispatch = useDispatch();
-  const filterProducts = () => dispatch(productsActions.filteringProducts(id));
+
+  const filterProducts = () => {
+    dispatch(productsActions.filteringProducts(id));
+    dispatch(categoriesActions.changeSelectedCategory(id));
+  };
+
+  const getSelectedCategoryId = useSelector(
+    state => state.categories.selectedCategoryId
+  );
+  const activeClass = getSelectedCategoryId === id ? "selected-category" : "";
   return (
-    <li onClick={filterProducts} className="list category">
+    <li onClick={filterProducts} className={`${activeClass} list category`}>
       {name}
     </li>
   );
