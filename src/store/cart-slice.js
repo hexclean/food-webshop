7e59d3dafe8abe77511bc52cb45a574d5a195c14
hexclean <erdosjozsef20@gmail.com>
2021-12-28 13:00:12 +0200

    Adding cart functionality

---
+++ src/store/cart-slice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalAmount: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+      state.totalQuantity++;
+      state.totalAmount += newItem.price;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalItemAmount: newItem.price,
+          name: newItem.name,
+          imageUrl: newItem.imageUrl,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalItemAmount += newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      state.totalAmount -= existingItem.price;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id != id);
+      } else {
+        existingItem.quantity--;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
